caf44b4ade0a3a6c22a80560d36b5a116c4a6080
Semyon Perepelitsa <sema@sema.in>
2011-12-15 10:50:27 +0800

    Refactoring: extracting Ball and Paddle classes

--- pong.rb
+++ pong.rb
@@ -12,63 +12,126 @@ java_import org.newdawn.slick.Input
 java_import org.newdawn.slick.SlickException
 java_import org.newdawn.slick.AppGameContainer
 
+
+class Ball
+  attr_accessor :x, :y, :angle, :game
+
+  def initialize(game)
+    @image = Image.new('media/ball.png')
+    @game = game
+    reset
+  end
+
+  def reset
+    @x = 200
+    @y = 200
+    @angle = 45
+  end
+
+  def width
+    @image.width
+  end
+
+  def height
+    @image.height
+  end
+
+  def render(container, graphics)
+    @image.draw(@x, @y)
+  end
+
+  def update(container, delta)
+    @x += 0.3 * delta * Math.cos(@angle * Math::PI / 180)
+    @y -= 0.3 * delta * Math.sin(@angle * Math::PI / 180)
+
+    if (@x > container.width - width) || (@y < 0) || (@x < 0)
+      @angle = (@angle + 90) % 360
+    end
+
+    if @y > container.height
+      game.reset
+    end
+  end
+
+end
+
+class Paddle
+  attr_reader :x, :y, :angle, :game
+
+  def initialize(game)
+    @image = Image.new('media/paddle.png')
+    @game = game
+    reset
+  end
+
+  def reset
+    @x = 200
+    @y = 400
+  end
+
+  def width
+    @image.width
+  end
+
+  def height
+    @image.height
+  end
+
+  def render(container, graphics)
+    @image.draw(@x, @y)
+  end
+
+  def update(container, delta)
+    input = container.get_input
+
+    if input.is_key_down(Input::KEY_LEFT) and @x > 0
+      @x -= 0.3 * delta
+    end
+
+    if input.is_key_down(Input::KEY_RIGHT) and @x < container.width - width
+      @x += 0.3 * delta
+    end
+  end
+
+end
+
+
 class PongGame < BasicGame
   def render(container, graphics)
     @bg.draw(0, 0)
-    @ball.draw(@ball_x, @ball_y)
-    @paddle.draw(@paddle_x, @paddle_y)
+    @ball.render(container, graphics)
+    @paddle.render(container, graphics)
     graphics.draw_string("RubyPong (ESC to exit)", 8, container.height - 30)
   end
 
   def init(container)
     @bg = Image.new('media/bg.png')
-
-    @ball = Image.new('media/ball.png')
-    @ball_x = 200
-    @ball_y = 200
-    @ball_angle = 45
-
-    @paddle = Image.new('media/paddle.png')
-    @paddle_x = 200
-    @paddle_y = 400
+    @ball = Ball.new(self)
+    @paddle = Paddle.new(self)
   end
 
   def update(container, delta)
     input = container.get_input
     container.exit if input.is_key_down(Input::KEY_ESCAPE)
 
-    if input.is_key_down(Input::KEY_LEFT) and @paddle_x > 0
-      @paddle_x -= 0.3 * delta
-    end
-
-    if input.is_key_down(Input::KEY_RIGHT) and @paddle_x < container.width - @paddle.width
-      @paddle_x += 0.3 * delta
-    end
-
-    @ball_x += 0.3 * delta * Math.cos(@ball_angle * Math::PI / 180)
-    @ball_y -= 0.3 * delta * Math.sin(@ball_angle * Math::PI / 180)
-
-    if (@ball_x > container.width - @ball.width) || (@ball_y < 0) || (@ball_x < 0)
-      @ball_angle = (@ball_angle + 90) % 360
-    end
-
-    if @ball_y > container.height
-      @paddle_x = 200
-      @ball_x = 200
-      @ball_y = 200
-      @ball_angle = 45
-    end
-
-    if  @ball_y + @ball.height > @paddle_y and
-        @ball_x < @paddle_x + @paddle.width and
-        @ball_x + @ball.width > @paddle_x
-      @ball_angle = (@ball_angle + 80 + rand(20) - 10) % 360
-    end
+    @ball.update(container, delta)
+    @paddle.update(container, delta)
 
     # if @ball_x >= @paddle_x and @ball_x <= (@paddle_x + @paddle.width) and @ball_y.round >= (400 - @ball.height)
     #   @ball_angle = (@ball_angle + 90) % 360
     # end
 
+    if  @ball.y + @ball.height > @paddle.y and
+        @ball.x < @paddle.x + @paddle.width and
+        @ball.x + @ball.width > @paddle.x
+      @ball.angle = (@ball.angle + 80 + rand(20) - 10) % 360
+    end
+
+  end
+
+  def reset
+    @ball.reset
+    @paddle.reset
   end
 end # class
 
